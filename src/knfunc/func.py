97aa87b39384814234b563bcb807e40fb7ee843e
Omg Bebebe <omgbebebe@gmail.com>
2024-08-30 08:46:36 +0000

    back to sdk master

--- src/knfunc/func.py
+++ src/knfunc/func.py
@@ -1,9 +1,25 @@
+import mindwm.model.graph as graph
 from mindwm import logging
-from mindwm.knfunc.decorators import kafka_cdc, app
+from mindwm.knfunc.decorators import Request, Response, app, event
+from mindwm.model.events import (KafkaCdc, MindwmEvent, from_request,
+                                 to_response)
 
 logger = logging.getLogger(__name__)
 
-@kafka_cdc
-async def func(event):
-    logger.info(f"event: {event}")
-    return event.data
+
+@event
+async def mindwm_cdc(obj: KafkaCdc, request: Request, response: Response):
+    cdc_ev = await from_request(request)
+    res = graph.GraphObjectChanged.from_kafka_cdc(cdc_ev.data)
+
+    new_ev = MindwmEvent(
+        source=f"org.mindwm.context.cyan.knfunc.kafka_cdc",
+        subject=f"org.mindwm.context.cyan.graph.{cdc_ev.data.payload.type}",
+        type=res.type,
+        data=res,
+        traceparent=res.obj.traceparent,
+    )
+    resp = to_response(new_ev)
+    logger.info(f"Headers: {resp.headers}")
+    logger.info(f"Body: {resp.body}")
+    return resp
